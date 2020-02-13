7b69217447114faab1731431ca2b90eb07b3f359
Alfred Doh-Nani <alfred.doh-nani@meltwater.org>
2020-02-13 08:27:17 +0000

    Correct timer recursion

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2020,
+                month: 2,
+                day: 13,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
